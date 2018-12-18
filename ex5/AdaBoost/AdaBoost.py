73591333c82ef37e5b8d26e924b300e5035a8446
Treekay <823334594@qq.com>
2018-12-18 08:45:29 +0800

    final

--- ex5/AdaBoost/AdaBoost.py
+++ ex5/AdaBoost/AdaBoost.py
@@ -48,12 +48,33 @@ class AdaBoost(object):
         discrete_estimator_errors = bdt_discrete.estimator_errors_[:n_trees_discrete]
         discrete_estimator_weights = bdt_discrete.estimator_weights_[:n_trees_discrete]
 
-        plt.figure(figsize=(5, 5))
+        plt.figure(figsize=(15, 5))
+        plt.subplot(131)
         plt.plot(range(1, n_trees_discrete + 1), discrete_test_errors, c='black', label='SAMME')
+        plt.annotate('%.3f' % discrete_test_errors[-1], xy=(n_trees_discrete, discrete_test_errors[-1]))
         plt.legend()
         plt.ylim(0, 1.00)
         plt.ylabel('Test Error')
         plt.xlabel('Number of Trees')
+
+        plt.subplot(132)
+        plt.plot(range(1, n_trees_discrete + 1), discrete_estimator_errors, "b", label='SAMME', alpha=.5)
+        plt.legend()
+        plt.ylabel('Error')
+        plt.xlabel('Number of Trees')
+        plt.ylim((.2, discrete_estimator_errors.max() * 1.2))
+        plt.xlim((-20, len(bdt_discrete) + 20))
+
+        plt.subplot(133)
+        plt.plot(range(1, n_trees_discrete + 1), discrete_estimator_weights, "b", label='SAMME')
+        plt.legend()
+        plt.ylabel('Weight')
+        plt.xlabel('Number of Trees')
+        plt.ylim((0, discrete_estimator_weights.max() * 1.2))
+        plt.xlim((-20, n_trees_discrete + 20))
+
+        # prevent overlapping y-axis labels
+        plt.subplots_adjust(wspace=0.25)
         plt.show()
         
 if __name__ == '__main__':
